a356da7efb88b1d27cea4f38317622288cbbdd87
Christian Jöcker <christian.joecker@maibornwolff.de>
2022-11-30 07:42:59 +0100

    add planets and starships list

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import './App.css'
-import {List} from './components/list'
+import { List } from './components/list'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <List type={'people'} />,
+  },
+  {
+    path: '/planets',
+    element: <List type={'planets'} />,
+  },
+])
 
 function App() {
-
-
-  return (
-    <div className="font-bold text-base">
-      <List type="people"/>
-    </div>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
